85598f9ee6e60782f77181c5522b6211f6958d75
fran028 <60265684+fran028@users.noreply.github.com>
2024-12-24 13:27:17 +0000

    Initial commit

---
+++ app/src/main/java/com/example/footballstatistics_app_wearos/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.footballstatistics_app_wearos.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun FootballStatistics_App_WearOSTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
